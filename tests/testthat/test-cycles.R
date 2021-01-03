57e3290012c65dd52d52cf31211cf1970bdd5b92
mpadge <mark.padgham@email.com>
2021-01-03 22:37:43 +0100

    add first tests

---
+++ tests/testthat/test-cycles.R
@@ -0,0 +1,15 @@
+
+test_that("cycles", {
+
+    library (dodgr)
+
+    net <- dodgr::weight_streetnet (hampi_sc, wt_profile = "foot")
+    net <- net [net$component == 1, ]
+    netc <- dodgr::dodgr_contract_graph (net)
+    netc$flow <- 1
+    x <- dodgr::merge_directed_graph (netc)
+
+    expect_message (paths <- ltn_cycles (x))
+    expect_type (paths, "list")
+    expect_equal (length (paths), 49)
+})
