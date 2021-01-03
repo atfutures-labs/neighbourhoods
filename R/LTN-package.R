db1495ba283e112a72018b25d6d9203a91f5c3ef
mpadge <mark.padgham@email.com>
2021-01-03 22:35:23 +0100

    add data/hampi_sc

--- R/LTN-package.R
+++ R/LTN-package.R
@@ -8,3 +8,14 @@
 #' @importFrom Rcpp sourceCpp
 ## usethis namespace: end
 NULL
+
+#' hampi_sc
+#'
+#' A sample street network from the primarily pedeestrian township of Hampi,
+#' Karnataka, India.
+#'
+#' @name hampi_sc
+#' @docType data
+#' @keywords datasets
+#' @format A \pkg{silicate} representation of the way network of Hampi.
+NULL
